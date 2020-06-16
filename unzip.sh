71ffd610f7921140191e13f00289cb53390cb4a0
Eric Crawford <eric.crawford@mail.mcgill.ca>
2020-06-16 14:16:47 -0400

    Added first round of rollouts.

---
+++ unzip.sh
@@ -0,0 +1,5 @@
+mkdir atari_rollouts
+unzip asteroids.zip -d atari_rollouts
+unzip carnival.zip -d atari_rollouts
+unzip space_invaders.zip -d atari_rollouts
+unzip wizard_of_wor.zip -d atari_rollouts
